347b005b6382c06cf76503c4fcf4d7b7e28662c5
richardgohcr <richard.gohcr@gmail.com>
2020-05-29 02:28:35 +0800

    Added carts page

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+
+
 const app = express();
 
 app.use(express.static('public'));
@@ -15,7 +19,9 @@ app.use(
   })
 );
 app.use(authRouter);
-app.use(productsRouter);
+app.use(adminProductsRouter);
+app.use(productsRouter)
+app.use(cartsRouter)
 
 app.listen(3000, () => {
   console.log('Listening');
