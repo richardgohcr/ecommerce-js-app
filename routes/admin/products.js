3abedf3a44b8d7a9a26f30d2d01b162212f5ff69
richardgohcr <richard.gohcr@gmail.com>
2020-05-28 12:41:01 +0800

    Added ability to add new products and quick solution for image upload

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { validationResult } = require('express-validator');
+const multer = require('multer');
 
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', (req, res) => {});
 
@@ -13,9 +15,23 @@ router.get('/admin/products/new', (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post('/admin/products/new', [requireTitle, requirePrice], (req, res) => {
-  console.log(validationResult(req));
-  res.send('Submitted');
-});
+router.post(
+  '/admin/products/new',
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.send(productsNewTemplate({ errors }));
+    }
+    const image = req.file.buffer.toString('base64');
+
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
+
+    res.send('Submitted');
+  }
+);
 
 module.exports = router;
