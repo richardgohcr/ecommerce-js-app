43be927bc5a75e65222d12c0cbe7e51c4a771d5b
richardgohcr <richard.gohcr@gmail.com>
2020-05-28 13:41:16 +0800

    Added support for adding products

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -2,17 +2,17 @@ const express = require('express');
 const { validationResult } = require('express-validator');
 const multer = require('multer');
 
-const { handleErrors , requireAuth} = require('./middlewares');
+const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/admin/products', requireAuth, async (req, res) => {
-
   const products = await productsRepo.getAll();
   res.send(productsIndexTemplate({ products }));
 });
@@ -33,6 +33,35 @@ router.post(
     await productsRepo.create({ title, price, image });
 
     res.redirect('/admin/products');
+  }
+);
+
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+  const product = await productsRepo.getOne(req.params.id);
+  if (!product) {
+    return res.send('Product not found');
+  }
+  res.send(productsEditTemplate({ product }));
+});
+
+router.post(
+  '/admin/products/:id/edit',
+  requireAuth,
+  upload.single('image'),
+  [requireTitle, requirePrice],
+  handleErrors(productsEditTemplate),
+  async (req, res) => {
+    const changes = req.body;
+    if (req.file) {
+      changes.image = req.file.buffer.toString('base64');
+    }
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      return res.send('Count not find item');
+    }
+
+    res.redirect('/admin/products')
   }
 );
 
