d97aa016121e05188d823bfffbcbe1509e428987
richardgohcr <richard.gohcr@gmail.com>
2020-05-29 00:02:26 +0800

    Add delete functionality

--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -8,14 +8,16 @@ module.exports = ({ products }) => {
         <td>${product.title}</td>
         <td>${product.price}</td>
         <td>
-          <a href="/admin/products/${product.id}/edit">
+          <a href="/admin/products/${product.id}">
             <button class="button is-link">
               Edit
             </button>
           </a>
         </td>
         <td>
+          <form method="POST" action="/admin/products/${product.id}/delete">
           <button class="button is-danger">Delete</button>
+          </form>
         </td>
       </tr>
     `;
